8b77e31b19a17c8cc50f50c30630951f427bce29
Thaymisan C <59618015+thaymisan@users.noreply.github.com>
2026-04-30 14:43:09 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'media', // automatically follows system preference
+  theme: { extend: {} },
+  plugins: [],
+}
